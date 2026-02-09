b5456ba46e48b0e48c78f61a554265eb3f983837
Brock <kbrock@ab.mpg.de>
2026-02-09 15:06:14 +0100

    get functionrunner passing check

--- inner-pkgs/functionrunner/R/runfunction.R
+++ inner-pkgs/functionrunner/R/runfunction.R
@@ -1,1 +1,6 @@
-run_function <- function (fn) fn()
+#' Extremely simplified version of tinysnapshot::expect_snapshot_plot
+#'
+#' Takes a function as an argument and runs that function
+#' @param fn function to be run
+#' @export
+run_function <- function(fn) fn()
